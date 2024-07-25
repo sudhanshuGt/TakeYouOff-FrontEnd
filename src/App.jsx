11d75b08a3b09575ad2bb1e083f2295a310af6a8
sudhanshuGt <sudhanshutiwari357@gmail.com>
2024-07-25 11:59:06 +0530

    added google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,30 @@ import CreateJobs from './components/admin/CreateJobs'
 import ProtectedRoute from './components/admin/ProtectedRoute'
 import Applicants from './components/admin/Applicants'
 
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAQVQJ8OpcVx36QVu_oP5tUt8UHYdiZnJc",
+  authDomain: "inscribe-d9e3b.firebaseapp.com",
+  projectId: "inscribe-d9e3b",
+  storageBucket: "inscribe-d9e3b.appspot.com",
+  messagingSenderId: "1051026563088",
+  appId: "1:1051026563088:web:5c71666886cf14058eaa95",
+  measurementId: "G-QBMSG5H51V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const appRouter = createBrowserRouter([
   {
     path: "/",
@@ -70,6 +94,10 @@ const appRouter = createBrowserRouter([
     element:<ProtectedRoute><CompanySetup/></ProtectedRoute> 
   },
 ])
+
+
+
+
 
 function App() {
   return (
